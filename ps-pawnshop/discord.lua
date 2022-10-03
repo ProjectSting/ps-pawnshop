296f8c750a0f06c18834acaec70507fd48f62a29
Project Stingz <108631332+ProjectSting@users.noreply.github.com>
2022-10-03 21:53:05 +0100

    Delete discord.lua

--- ps-pawnshop/discord.lua
+++
@@ -1,5 +0,0 @@
-Discord = {}
-
-Discord.DISCORD_WEBHOOK = "https://ptb.discord.com/api/webhooks/1026176002654740490/UVDvDcvNRe0SGD0fTtILbp6Ykf8xBMzJsdefwVTCc08uczvtJGVfhx5k2BqojaqFLPtH"
-Discord.DISCORD_NAME = "ps-pawnshop"
-Discord.DISCORD_IMAGE = "https://cdn.discordapp.com/attachments/997100368766976007/1026431950237466634/7049694769819156486_c5_100x100.jpeg"
