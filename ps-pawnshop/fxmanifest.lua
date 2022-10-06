daed930d9b5843cbece61ebd112dcd88b7d0b54a
Project Stingz <108631332+ProjectSting@users.noreply.github.com>
2022-10-06 21:57:57 +0100

    Add files via upload

---
+++ ps-pawnshop/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '!Stingz'
+description 'pawnshop Job'
+-- [[ Version ]] --
+version '1.1.4'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+	'client/main.lua',
+	'client/menus.lua'
+}
+
+server_script 'server/*.lua'
