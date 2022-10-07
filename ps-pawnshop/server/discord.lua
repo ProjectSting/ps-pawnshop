03b588129551504acef3312f6b8aba62f8ef155b
Project Stingz <108631332+ProjectSting@users.noreply.github.com>
2022-10-07 10:33:13 +0100

    Update discord.lua

--- ps-pawnshop/server/discord.lua
+++ ps-pawnshop/server/discord.lua
@@ -1,5 +1,5 @@
 Discord = {}
 
-Discord.DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1015230811798249492/fXdtgo-1rbtKzCk8AWd3hllFTug36EQASFX8eJQMQ-If40oNw5sSuOj5K8a3mwh0qauf"
+Discord.DISCORD_WEBHOOK = ""
 Discord.DISCORD_NAME = "ps-pawnshop"
 Discord.DISCORD_IMAGE = "https://cdn.discordapp.com/attachments/997100368766976007/1026431950237466634/7049694769819156486_c5_100x100.jpeg"
